4845ca9ca8a66320887fdf55454d960cd90e572c
yshujie <yshujie@163.com>
2024-05-28 12:48:11 +0800

    Test try print Hello World

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from datatime import datetime
+
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return {"message": f"Hello World, current time is {current_time}"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
